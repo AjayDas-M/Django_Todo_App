391aed6d9cdcd48f34b9e25ccee4400072388de4
Ajay Das <ajaydasmanningattil@gmail.com>
2026-02-23 00:48:05 +0530

    updated navigation bar

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,15 @@
-from django.forms import ModelForm, ImageField,FileInput
-from .models import Blog, Profile
+from django import forms
+from .models import Blog
 
-
-class BlogForm(ModelForm):
-    image = ImageField(required=False)
+class BlogForm(forms.ModelForm):
+    image = forms.ImageField(required=False)
 
     class Meta:
         model = Blog
-        # fields = "__all__"
         exclude = ('user', 'likes')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # This loop adds the Bootstrap class to every field automatically
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
